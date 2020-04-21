2b4460c23d209848919b6e97ee777023b31eaf55
pylagg <62642609+pylagg@users.noreply.github.com>
2020-04-21 15:43:28 +0530

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -24,5 +24,38 @@ def call(){
 				 bat 'mvn test'
 			}
 		}
-  }}
+	  	stage('build && SonarQube analysis') 
+		{
+            		steps {
+                		withSonarQubeEnv('sonar') {
+                                     		bat 'mvn sonar:sonar'
+                		}
+            		}
+        	}    
+		stage('Deploy artifact using maven')
+		{
+			steps{
+			bat 'mvn deploy'
+			//stash name: 'war', includes: '**/*.war'
+			
+			}
+		}
+		       
+  }  
+
+	post {
+		 
+   		 failure {
+      			  mail to: mail_id,
+             		subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+             		body: "Something is wrong with ${env.BUILD_URL}"
+    		}
+		success{
+      			  mail to: mail_id,
+             		subject: "Success Pipeline: ${currentBuild.fullDisplayName}",
+             		body: "Congratulations ${env.BUILD_URL}"
+    		}
+		}
+	
+}
 }
