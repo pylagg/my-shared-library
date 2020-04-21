ac4a504c7ceaa85185408c5b4d27ac7b95aa7d48
pylagg <62642609+pylagg@users.noreply.github.com>
2020-04-21 14:32:36 +0530

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -1,5 +1,5 @@
 // Configure using microservice-pipelines and using "part2" branch
-@Library("microservice-pipelines@part2") _
+@Library("my-shared-library@part2") _
 
 // Entry point into microservice-pipelines
 jenkinsJob.call()
