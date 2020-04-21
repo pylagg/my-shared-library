57e4e0a98fc6934d6273c9cfe3a1244aad976228
pylagg <62642609+pylagg@users.noreply.github.com>
2020-04-21 18:12:15 +0530

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -36,41 +36,10 @@ def call(){
 		{
 			steps{
 			bat 'mvn deploy'
-			stash name: 'war', includes: '**/*.war'
+			//stash name: 'war', includes: '**/*.war'
 			}
 		}
-		stage('Building image') {
- 			 agent {
-			 label 'slave_docker'
-			 }
-			 steps{
-				unstash 'war'
-				sh 'docker build -t pylagg/first_repo":${BUILD_NUMBER}" .'
-			}
-  		}
-		stage('Deploy image') {
-			agent {
-			label 'slave_docker'
-			}
-			steps{
-				sh 'docker push pylagg/first_repo":${BUILD_NUMBER}"'
-			}
-		}
-		stage('Run container'){
-			agent {
-			label 'slave_docker'
-			}
-			steps{
-				sh( script: '''#!/bin/bash
-           				 if [ "$(docker ps -aq -f name=con1)" ]; then
-					docker stop con1
-					docker rm con1
-					fi
-        				'''.stripIndent())
-				sh 'docker run -d --name con1 -p 80:8080 pylagg/first_repo":${BUILD_NUMBER}"'
-			}
-		}
-	
+		
 }
 }
 }
